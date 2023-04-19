db68aeb5a4c8257fa9bc0164d4cdce09a25ae1d5
KangKang <kangzixiang@foxmail.com>
2023-04-19 19:02:25 +0800

    Create SimpleHDLViewer.cpp

---
+++ SimpleHDLViewer.cpp
@@ -0,0 +1,60 @@
+#include "SimpleHDLViewer.h"
+
+SimpleHDLViewer::SimpleHDLViewer(pcl::Grabber &grabber,
+                  PointCloudColorHandler<pcl::PointXYZI> &handler) :
+                  cloud_viewer_(new PCLVisualizer("PCL HDL Cloud")),
+                  grabber_(grabber),
+                  handler_(handler)
+{
+}
+
+void SimpleHDLViewer::cloud_callback(const CloudConstPtr &cloud)
+{
+    std::lock_guard<std::mutex> lock(cloud_mutex_);
+    cloud_ = cloud;
+}
+
+void SimpleHDLViewer::run()
+{
+    cloud_viewer_->addCoordinateSystem(3.0);
+    cloud_viewer_->setBackgroundColor(0, 0, 0);
+    cloud_viewer_->initCameraParameters();
+    cloud_viewer_->setCameraPosition(0.0, 0.0, 30.0, 0.0, 1.0, 0.0, 0);
+    cloud_viewer_->setCameraClipDistances(0.0, 50.0);
+
+    std::function<void (const CloudConstPtr&)> cloud_cb =
+        [this] (const CloudConstPtr &cloud) {cloud_callback(cloud);};
+    boost::signals2::connection cloud_connection = grabber_.registerCallback(cloud_cb);
+
+    grabber_.start();
+    
+    while (!cloud_viewer_->wasStopped())
+    {
+        CloudConstPtr cloud;
+
+        // See if we can get a cloud
+        if (cloud_mutex_.try_lock())
+        {
+            cloud_.swap(cloud);
+            cloud_mutex_.unlock();
+        }
+
+        if (cloud)
+        {
+            handler_.setInputCloud(cloud);
+            if (!cloud_viewer_->updatePointCloud(cloud, handler_, "HDL"))
+                cloud_viewer_->addPointCloud(cloud, handler_, "HDL");
+
+            cloud_viewer_->spinOnce();
+        }
+
+        if (!grabber_.isRunning())
+            cloud_viewer_->spin();
+
+        std::this_thread::sleep_for(100us);
+    }
+
+    grabber_.stop();
+
+    cloud_connection.disconnect();
+}
