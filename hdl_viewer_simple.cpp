90ec73eb9e3721dbf6b399667e1a507b84d570ce
KangKang <kangzixiang@foxmail.com>
2023-04-19 19:02:48 +0800

    Create hdl_viewer_simple.cpp

---
+++ hdl_viewer_simple.cpp
@@ -0,0 +1,21 @@
+
+#include "SimpleHDLViewer.h"
+#include <pcl/io/vlp_grabber.h>
+
+
+int main(int argc, char ** argv)
+{
+  std::string hdlCalibration, pcapFile;
+
+  parse_argument(argc, argv, "-calibrationFile", hdlCalibration);
+  parse_argument(argc, argv, "-pcapFile", pcapFile);
+
+  // pcapFile = "HDL32-V2_Monterey Highway.pcap";
+  pcl::HDLGrabber grabber(hdlCalibration, pcapFile);
+  // pcl::VLPGrabber g(pcapFile);
+  PointCloudColorHandlerGenericField<pcl::PointXYZI> color_handler("intensity");
+
+  SimpleHDLViewer v(grabber, color_handler);
+  v.run();
+  return 0;
+}
